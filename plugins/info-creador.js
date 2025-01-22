36c323d78b6f1254f7de90ef78eaf0085838d601
gudo1234 <izumilitee@gmail.com>
2025-01-22 12:34:03 -0600

    Add files via upload

---
+++ plugins/info-creador.js
@@ -0,0 +1,31 @@
+function handler(m) {
+let name = conn.getName(`${numcreador}@s.whatsapp.net`)
+let ownerN = `${numcreador}`
+conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
+ contextInfo: { 
+ forwardingScore: 2023,
+isForwarded: false, 
+ externalAdReply: {  
+ title: packname, 
+ body: dev, 
+ sourceUrl: channel,
+ thumbnail: banner,
+ thumbnailUrl: banner, 
+ mediaType: 1,
+ showAdAttribution: true, 
+ renderLargerThumbnail: true 
+ }
+   }
+     },
+       {
+         quoted: m
+           }
+             );
+
+}
+
+handler.help = ['owner']
+handler.tags = ['main']
+handler.command = ['owner', 'creator', 'creador', 'dueño'] 
+
+export default handler
