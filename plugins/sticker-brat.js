d5f4f93d90198af305ce909d7196fa4b45bb5b20
gudo1234 <izumilitee@gmail.com>
2025-01-22 16:04:23 -0600

    Update sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/*import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
 import sharp from 'sharp';
@@ -76,4 +76,4 @@ handler.command = ['brat'];
 handler.tags = ['sticker'];
 handler.help = ['brat *<texto>*'];
 
-export default handler;
+export default handler;*/
