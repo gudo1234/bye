9aa55fece8dec300d446e942e0afd0670010fb07
gudo1234 <izumilitee@gmail.com>
2025-01-25 10:51:48 -0600

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -45,13 +45,13 @@ let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch
   };
   let who = m.messageStubParameters[0] + '@s.whatsapp.net';
   let userName = user ? user.name : await conn.getName(who);
-  let bienvenida = `${e} Bienvenido *@${m.messageStubParameters[0].split`@`[0]}*\n${global.welcom1}\n ٩(๑❛ᴗ❛๑)۶ Disfruta tu estadía en el grupo!\n> ✎Puedes usar *#menu* para ver la lista de comandos.`
-  let bye = `${e} Adios *@${m.messageStubParameters[0].split`@`[0]}*\n${global.welcom2}\n ٩(๑❛ᴗ❛๑)۶ Te esperamos pronto!\n> ✎Puedes usar *#menu* para ver la lista de comandos.`
+  //let bienvenida = `${e} Bienvenido *@${m.messageStubParameters[0].split`@`[0]}*\n${global.welcom1}\n ٩(๑❛ᴗ❛๑)۶ Disfruta tu estadía en el grupo!\n> ✎Puedes usar *#menu* para ver la lista de comandos.`
+  //let bye = `${e} Adios *@${m.messageStubParameters[0].split`@`[0]}*\n${global.welcom2}\n ٩(๑❛ᴗ❛๑)۶ Te esperamos pronto!\n> ✎Puedes usar *#menu* para ver la lista de comandos.`
 
 // Welcome 
 if (chat.welcome && m.messageStubType == 27) {
 await conn.sendMessage(m.chat, {
-      text: bienvenida,
+      text: `${e} Bienvenido *@${m.messageStubParameters[0].split`@`[0]}*\n${global.welcom1}\n ٩(๑❛ᴗ❛๑)۶ Disfruta tu estadía en el grupo!\n> ✎Puedes usar *#menu* para ver la lista de comandos.`,
       contextInfo: {
           mentionedJid: [m.messageStubParameters[0]],
           isForwarded: true,
@@ -78,7 +78,7 @@ await conn.sendMessage(m.chat, {
 // bye 
   if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
 await conn.sendMessage(m.chat, {
-      text: bye,
+      text: `${e} Adios *@${m.messageStubParameters[0].split`@`[0]}*\n${global.welcom2}\n ٩(๑❛ᴗ❛๑)۶ Te esperamos pronto!\n> ✎Puedes usar *#menu* para ver la lista de comandos.`,
       contextInfo: {
           mentionedJid: [m.messageStubParameters[0]],
           isForwarded: true,
