da3b0107cce03c8df06f096493b1f6aa1adfeb77
gudo1234 <izumilitee@gmail.com>
2025-01-25 07:49:13 -0600

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -20,10 +20,52 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
       let bienvenida = `${e} *Bienvenido* a ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom1}\n •(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ❖Puedes usar *#menu* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
+      //await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
+      await conn.sendMessage(m.chat, { 
+      text: bienvenida,
+      contextInfo: {
+          mentionedJid: [who],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+          forwardingScore: 999,
+          externalAdReply: {
+              title: `${await conn.getName(m.chat)}`,
+              body: `${botname} te da la bienvenida 🎉`,
+              thumbnailUrl: img,
+              mediaType: 1,
+              showAdAttribution: true,
+              renderLargerThumbnail: true,
+          },
+      },
+  }, { quoted: null });
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
       let bye = `${e} *Adiós* De ${groupMetadata.subject}\n ✰ ${taguser}\n${global.welcom2}\n •(=^●ω●^=)• Te esperamos pronto!\n> ❖Puedes usar *#menu* para ver la lista de comandos.`
-      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
+      //await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
+   await conn.sendMessage(m.chat, { 
+      text: bye,
+      contextInfo: {
+          mentionedJid: [who],
+          isForwarded: true,
+          forwardedNewsletterMessageInfo: {
+              newsletterJid: channelRD.id,
+              newsletterName: channelRD.name,
+              serverMessageId: -1,
+          },
+          forwardingScore: 999,
+          externalAdReply: {
+              title: botname,
+              body: 'Esperemos que no vuelva -_-',
+              thumbnailUrl: img,
+              mediaType: 1,
+              showAdAttribution: true,
+              renderLargerThumbnail: true,
+          },
+      },
+  }, { quoted: null });
     }
   }
 
