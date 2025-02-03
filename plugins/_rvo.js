3bb6ac4ddf0c9b3305e55dc429b96790e18cfbdd
gudo1234 <izumilitee@gmail.com>
2025-02-02 19:56:38 -0600

    Update _rvo.js

--- plugins/_rvo.js
+++ plugins/_rvo.js
@@ -1,21 +1,31 @@
 let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
-if (!m.quoted) return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
-if (m.quoted.mtype !== 'viewOnceMessageV2') return conn.reply(m.chat, `${e} Responde a una imagen ViewOnce.`, m)
-let msg = m.quoted.message
-let type = Object.keys(msg)[0]
-let media = await downloadContentFromMessage(msg[type], type == 'imageMessage' ? 'image' : 'video')
-let buffer = Buffer.from([])
-for await (const chunk of media) {
-buffer = Buffer.concat([buffer, chunk])
-}
-if (/video/.test(type)) {
-return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m, null, rcanal)
-} else if (/image/.test(type)) {
-return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m, null, rcanal)
-}}
-handler.command = ['readviewonce', 'read', 'ver', 'readvo','ewonce','rvo', 'view']
-//handler.group = true;
+    if (!m.quoted) return conn.reply(m.chat, `⚠️ Responde a una imagen ViewOnce.`, m);
+    if (m.quoted.mtype !== 'viewOnceMessageV2') return conn.reply(m.chat, `⚠️ Responde a una imagen ViewOnce.`, m);
+    
+    let msg = m.quoted.message;
+    let type = Object.keys(msg)[0];
+    
+    // Asegúrate de que el tipo sea correcto
+    if (!['imageMessage', 'videoMessage'].includes(type)) {
+        return conn.reply(m.chat, `⚠️ El mensaje no es una imagen o video ViewOnce.`, m);
+    }
 
-export default handler
+    let media = await downloadContentFromMessage(msg[type], type === 'imageMessage' ? 'image' : 'video');
+    let buffer = Buffer.from([]);
+    
+    for await (const chunk of media) {
+        buffer = Buffer.concat([buffer, chunk]);
+    }
+
+    if (/video/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m, null);
+    } else if (/image/.test(type)) {
+        return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m, null);
+    }
+}
+
+handler.command = ['readviewonce', 'read', 'ver', 'readvo', 'ewonce', 'rvo', 'view'];
+
+export default handler;
