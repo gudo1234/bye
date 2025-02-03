50e3f574fa526d7480df57e7271401104999b576
gudo1234 <izumilitee@gmail.com>
2025-02-02 20:15:48 -0600

    Update _rvo.js

--- plugins/_rvo.js
+++ plugins/_rvo.js
@@ -6,26 +6,27 @@ let handler = async (m, { conn }) => {
     // Verificamos si el mensaje citado es un ViewOnce
     let quotedMsg = m.quoted.message;
     if (!quotedMsg || !quotedMsg.viewOnceMessage) {
-        return conn.reply(m.chat, `Responde a una imagen o video ViewOnce.`, m);
+        return conn.reply(m.chat, `⚠️ Responde a una imagen o video ViewOnce.`, m);
     }
 
     // Obtenemos el tipo de mensaje
-    let type = Object.keys(quotedMsg.viewOnceMessage)[0];
+    let viewOnceMsg = quotedMsg.viewOnceMessage;
+    let type = Object.keys(viewOnceMsg)[0];
 
     // Asegúrate de que el tipo sea correcto
     if (!['imageMessage', 'videoMessage'].includes(type)) {
-        return conn.reply(m.chat, `El mensaje no es una imagen o video ViewOnce.`, m);
+        return conn.reply(m.chat, `⚠️ El mensaje no es una imagen o video ViewOnce.`, m);
     }
 
     // Descargamos el contenido del mensaje
-    let media = await downloadContentFromMessage(quotedMsg.viewOnceMessage, type === 'imageMessage' ? 'image' : 'video');
+    let media = await downloadContentFromMessage(viewOnceMsg, type === 'imageMessage' ? 'image' : 'video');
     let buffer = Buffer.concat(await Promise.all([...media]));
 
     // Enviamos el archivo
     if (/video/.test(type)) {
-        return conn.sendFile(m.chat, buffer, 'media.mp4', quotedMsg.viewOnceMessage[type].caption || '', m, null);
+        return conn.sendFile(m.chat, buffer, 'media.mp4', viewOnceMsg[type].caption || '', m, null);
     } else if (/image/.test(type)) {
-        return conn.sendFile(m.chat, buffer, 'media.jpg', quotedMsg.viewOnceMessage[type].caption || '', m, null);
+        return conn.sendFile(m.chat, buffer, 'media.jpg', viewOnceMsg[type].caption || '', m, null);
     }
 }
 
