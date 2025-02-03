7fbd09833d113895142dd6af2fdc6c9a1555640b
gudo1234 <izumilitee@gmail.com>
2025-02-02 19:50:15 -0600

    Update _rvo.js

--- plugins/_rvo.js
+++ plugins/_rvo.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn }) => {
-    if (!m.quoted) return conn.reply(m.chat, '🛑 Responde a una imagen ViewOnce.', m)
+    if (!m.quoted) return conn.reply(m.chat, '🛑 Responde a una imagen o video ViewOnce.', m)
 
     const msg = m.quoted
     const buffer = await msg.download()
 
-    // Para video
-    if (msg.type === 'video') {
-        conn.sendFile(m.chat, buffer, 'media.mp4', msg.caption || '', m, null, { viewOnce: true })
+    // Verificamos si el mensaje es un video
+    if (msg.type === 'video' && msg.isViewOnce) {
+        conn.sendFile(m.chat, buffer, 'media.mp4', msg.caption || '', m, null, { viewOnce: false }) // Cambiamos a viewOnce: false para reenviar
     }
-    // Para foto
-    else if (msg.type === 'image') {
-        conn.sendFile(m.chat, buffer, 'media.jpg', msg.caption || '', m, null, { viewOnce: true })
+    // Verificamos si el mensaje es una imagen
+    else if (msg.type === 'image' && msg.isViewOnce) {
+        conn.sendFile(m.chat, buffer, 'media.jpg', msg.caption || '', m, null, { viewOnce: false }) // Cambiamos a viewOnce: false para reenviar
     } else {
         return conn.reply(m.chat, '❌ Solo se puede enviar imágenes o videos ViewOnce.', m)
     }
