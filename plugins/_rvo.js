029ce62895b567828317492311b8379e4b637162
gudo1234 <izumilitee@gmail.com>
2025-02-02 20:05:28 -0600

    Update _rvo.js

--- plugins/_rvo.js
+++ plugins/_rvo.js
@@ -2,11 +2,13 @@ let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
 let handler = async (m, { conn }) => {
     if (!m.quoted) return conn.reply(m.chat, `⚠️ Responde a una imagen o video ViewOnce.`, m);
-    if (!m.quoted.isViewOnce) return conn.reply(m.chat, `⚠️ Responde a una imagen o video ViewOnce.`, m);
-    
+    if (m.quoted.mtype !== 'viewOnceMessageV2' && m.quoted.mtype !== 'viewOnceMessage') {
+        return conn.reply(m.chat, `⚠️ Responde a una imagen o video ViewOnce.`, m);
+    }
+
     let msg = m.quoted.message;
     let type = Object.keys(msg)[0];
-    
+
     // Asegúrate de que el tipo sea correcto
     if (!['imageMessage', 'videoMessage'].includes(type)) {
         return conn.reply(m.chat, `⚠️ El mensaje no es una imagen o video ViewOnce.`, m);
@@ -14,7 +16,7 @@ let handler = async (m, { conn }) => {
 
     let media = await downloadContentFromMessage(msg[type], type === 'imageMessage' ? 'image' : 'video');
     let buffer = Buffer.from([]);
-    
+
     for await (const chunk of media) {
         buffer = Buffer.concat([buffer, chunk]);
     }
